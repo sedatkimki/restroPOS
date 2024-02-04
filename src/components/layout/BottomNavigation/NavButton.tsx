2632a24fffe4236ba00fcceaafb9315356ce75ef
sedat Korkmaz <68056590+sedatkimki@users.noreply.github.com>
2024-02-04 13:54:00 +0300

    feat: add bottom navigation

---
+++ src/components/layout/BottomNavigation/NavButton.tsx
@@ -0,0 +1,30 @@
+import { cn } from "@/lib/utils";
+import React from "react";
+import { Link, useLocation } from "react-router-dom";
+
+type NavButtonProps = {
+	icon: React.ReactNode;
+	name: string;
+	path: string;
+};
+
+const NavButton = ({ icon, name, path }: NavButtonProps) => {
+	const location = useLocation();
+	const isCurrentPath = location.pathname.includes(path);
+
+	const variant = isCurrentPath ? "text-primary " : "text-muted-foreground";
+	return (
+		<Link
+			className={cn(
+				"hover:bg-accent hover:text-primary text-muted-foreground inline-flex flex-col items-center justify-center px-5 gap-1",
+				variant,
+			)}
+			to={path}
+		>
+			{icon}
+			<div className="text-sm">{name}</div>
+		</Link>
+	);
+};
+
+export default NavButton;
---
+++ src/components/layout/BottomNavigation/index.tsx
@@ -0,0 +1,23 @@
+import NavButton from "./NavButton";
+import { NavigationLink } from "@/lib/types";
+
+type BottomNavigationProps = {
+	links: NavigationLink[];
+};
+
+export const BottomNavigation = ({ links }: BottomNavigationProps) => {
+	return (
+		<nav className="fixed bottom-0 left-0 z-50 w-full h-16 bg-white border-t">
+			<div className="grid h-full max-w-lg grid-cols-4 mx-auto">
+				{links.map((link) => (
+					<NavButton
+						key={link.name}
+						name={link.name}
+						path={link.path}
+						icon={link.icon}
+					/>
+				))}
+			</div>
+		</nav>
+	);
+};
